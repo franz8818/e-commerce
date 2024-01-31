57a2a09d56fc2e312b4c5e4c75b9204f443e3fb1
Franz Seidel <franz8818@gmail.com>
2024-01-31 11:08:35 -0500

    JS 1.0

---
+++ main.js
@@ -0,0 +1,23 @@
+
+//Funcion nativa
+// alert("HOLA FRANZ");
+
+//Variables
+let nombre = "Franz Seidel"
+nombre = "franz seidel"
+
+//Constantes
+const apellido = "Seidel"
+const altura = 165;
+altura = 180;
+
+//Concatenación -> Puedo unir dos variables
+let concatenacion = nombre + " " + Seidel; 
+
+//Seleccionar elementos de la página
+let datos =document.querySelector("#datos")
+
+//mostrar por consola
+console.log(nombre);
+console.log(altura);
+
